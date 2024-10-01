53d317e625ac6b5d1f45bc8f37982f7e09f077a1
Evan Kaiser <733581+holotrek@users.noreply.github.com>
2024-10-01 09:05:32 -0400

    Adds BMC button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,20 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${bravura.variable} ${bravuraText.variable} antialiased`}
       >
         {children}
+
+        <script
+          defer
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="honeydoodat"
+          data-description="Support me on Buy me a coffee!"
+          data-message="Hey 👋 thanks for visiting! If you like this free site, buy me a coffee!"
+          data-color="#5F7FFF"
+          data-position="Right"
+          data-x_margin="18"
+          data-y_margin="18"
+        />
       </body>
     </html>
   );
